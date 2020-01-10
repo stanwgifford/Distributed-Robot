9734de8391f0956a15e86c2b80be691e5ead2337
Stan Gifford <stan@gifford.id.au>
2020-01-11 10:35:08 +1100

    adding some source

--- camera/camera.c
+++ camera/camera.c
@@ -7,8 +7,6 @@
 #include <mosquitto.h>
 #include <unistd.h>
 
-
-
 #include <fcntl.h>
 #include <errno.h>
 #include <sys/ioctl.h>
@@ -18,166 +16,171 @@
 #include <linux/videodev2.h>
 #include <libv4l2.h>
 
-
-
 #define mqtt_host "mac-pro.local"
 #define mqtt_port 1883
 
 static int run = 1;
-volatile int stop=0;
-char *enabled="1";
+volatile int stop = 0;
+char *enabled = "1";
 
-struct buffer {
-        void   *start;
+struct buffer
+{
+        void *start;
         size_t length;
 };
-     
 
 #define CLEAR(x) memset(&(x), 0, sizeof(x))
-
 
 static void xioctl(int fh, int request, void *arg)
 {
         int r;
 
-        do {
+        do
+        {
                 r = v4l2_ioctl(fh, request, arg);
         } while (r == -1 && ((errno == EINTR) || (errno == EAGAIN)));
 
-        if (r == -1) {
+        if (r == -1)
+        {
                 fprintf(stderr, "error %d, %s\n", errno, strerror(errno));
                 exit(EXIT_FAILURE);
         }
 }
 
-
-
-void sigalrm_handler( int sig )
+void sigalrm_handler(int sig)
 {
-    stop = 1;
+        stop = 1;
 }
 
 void handle_signal(int s)
 {
-	run = 0;
+        run = 0;
 }
 
 void connect_callback(struct mosquitto *mosq, void *obj, int result)
 {
-	printf("connect callback, rc=%d\n", result);
+        printf("connect callback, rc=%d\n", result);
 }
 
 void message_callback(struct mosquitto *mosq, void *obj, const struct mosquitto_message *message)
 {
-	bool match = 0;
-//	printf("got message '%.*s' for topic '%s'\n", message->payloadlen, (char*) message->payload, message->topic);
+        bool match = 0;
+        //	printf("got message '%.*s' for topic '%s'\n", message->payloadlen, (char*) message->payload, message->topic);
 
-	mosquitto_topic_matches_sub("Camera/Disable", message->topic, &match);
-	if (match) {
-		enabled="1";
-	}
-        mosquitto_topic_matches_sub("Camera/Enable", message->topic, &match);
-        if (match) {
-                enabled="2";
+        mosquitto_topic_matches_sub("Camera/Disable", message->topic, &match);
+        if (match)
+        {
+                enabled = "1";
         }
-
+        mosquitto_topic_matches_sub("Camera/Enable", message->topic, &match);
+        if (match)
+        {
+                enabled = "2";
+        }
 
         mosquitto_topic_matches_sub("Camera/Grab", message->topic, &match);
-        if (match) {
-                
-     struct v4l2_format              fmt;
-        struct v4l2_buffer              buf;
-        struct v4l2_requestbuffers      req;
-        enum v4l2_buf_type              type;
-        fd_set                          fds;
-        struct timeval                  tv;
-        int                             r, fd = -1;
-        unsigned int                    i, n_buffers;
-        char                            *dev_name = "/dev/video0";
-        
-        
-        struct buffer                   *buffers;
+        if (match)
+        {
 
-        fd = v4l2_open(dev_name, O_RDWR | O_NONBLOCK, 0);
-        if (fd < 0) {
-                perror("Cannot open device");
-                exit(EXIT_FAILURE);
-        }
+                struct v4l2_format fmt;
+                struct v4l2_buffer buf;
+                struct v4l2_requestbuffers req;
+                enum v4l2_buf_type type;
+                fd_set fds;
+                struct timeval tv;
+                int r, fd = -1;
+                unsigned int i, n_buffers;
+                char *dev_name = "/dev/video0";
 
-        CLEAR(fmt);
-        fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-        fmt.fmt.pix.width       = 640;
-        fmt.fmt.pix.height      = 480;
-        fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_JPEG;
-        fmt.fmt.pix.field       = V4L2_FIELD_INTERLACED;
-        xioctl(fd, VIDIOC_S_FMT, &fmt);
-        if (fmt.fmt.pix.pixelformat != V4L2_PIX_FMT_JPEG) {
-                printf("Libv4l didn't accept jpg format. Can't proceed.\n");
-                exit(EXIT_FAILURE);
-        }
-        if ((fmt.fmt.pix.width != 640) || (fmt.fmt.pix.height != 480))
-                printf("Warning: driver is sending image at %dx%d\n",
-                        fmt.fmt.pix.width, fmt.fmt.pix.height);
+                struct buffer *buffers;
 
-        CLEAR(req);
-        req.count = 2;
-        req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-        req.memory = V4L2_MEMORY_MMAP;
-        xioctl(fd, VIDIOC_REQBUFS, &req);
-
-        buffers = calloc(req.count, sizeof(*buffers));
-        for (n_buffers = 0; n_buffers < req.count; ++n_buffers) {
-                CLEAR(buf);
-
-                buf.type        = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-                buf.memory      = V4L2_MEMORY_MMAP;
-                buf.index       = n_buffers;
-
-                xioctl(fd, VIDIOC_QUERYBUF, &buf);
-
-                buffers[n_buffers].length = buf.length;
-                buffers[n_buffers].start = v4l2_mmap(NULL, buf.length,
-                              PROT_READ | PROT_WRITE, MAP_SHARED,
-                              fd, buf.m.offset);
-
-                if (MAP_FAILED == buffers[n_buffers].start) {
-                        perror("mmap");
+                fd = v4l2_open(dev_name, O_RDWR | O_NONBLOCK, 0);
+                if (fd < 0)
+                {
+                        perror("Cannot open device");
                         exit(EXIT_FAILURE);
                 }
-        }
 
-        for (i = 0; i < n_buffers; ++i) {
-                CLEAR(buf);
-                buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-                buf.memory = V4L2_MEMORY_MMAP;
-                buf.index = i;
-                xioctl(fd, VIDIOC_QBUF, &buf);
-        }
-        type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+                CLEAR(fmt);
+                fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+                fmt.fmt.pix.width = 640;
+                fmt.fmt.pix.height = 480;
+                fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_JPEG;
+                fmt.fmt.pix.field = V4L2_FIELD_INTERLACED;
+                xioctl(fd, VIDIOC_S_FMT, &fmt);
+                if (fmt.fmt.pix.pixelformat != V4L2_PIX_FMT_JPEG)
+                {
+                        printf("Libv4l didn't accept jpg format. Can't proceed.\n");
+                        exit(EXIT_FAILURE);
+                }
+                if ((fmt.fmt.pix.width != 640) || (fmt.fmt.pix.height != 480))
+                        printf("Warning: driver is sending image at %dx%d\n",
+                               fmt.fmt.pix.width, fmt.fmt.pix.height);
 
-        xioctl(fd, VIDIOC_STREAMON, &type);
-        for (i = 0; i < 1; i++) {
-                do {
-                        FD_ZERO(&fds);
-                        FD_SET(fd, &fds);
+                CLEAR(req);
+                req.count = 2;
+                req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+                req.memory = V4L2_MEMORY_MMAP;
+                xioctl(fd, VIDIOC_REQBUFS, &req);
 
-                        /* Timeout. */
-                        tv.tv_sec = 2;
-                        tv.tv_usec = 0;
-                        
+                buffers = calloc(req.count, sizeof(*buffers));
+                for (n_buffers = 0; n_buffers < req.count; ++n_buffers)
+                {
+                        CLEAR(buf);
 
-                        r = select(fd + 1, &fds, NULL, NULL, &tv);
-                } while ((r == -1 && (errno = EINTR)));
-                if (r == -1) {
-                        perror("select");
-                        return errno;
+                        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+                        buf.memory = V4L2_MEMORY_MMAP;
+                        buf.index = n_buffers;
+
+                        xioctl(fd, VIDIOC_QUERYBUF, &buf);
+
+                        buffers[n_buffers].length = buf.length;
+                        buffers[n_buffers].start = v4l2_mmap(NULL, buf.length,
+                                                             PROT_READ | PROT_WRITE, MAP_SHARED,
+                                                             fd, buf.m.offset);
+
+                        if (MAP_FAILED == buffers[n_buffers].start)
+                        {
+                                perror("mmap");
+                                exit(EXIT_FAILURE);
+                        }
                 }
 
-                CLEAR(buf);
-                buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-                buf.memory = V4L2_MEMORY_MMAP;
-                xioctl(fd, VIDIOC_DQBUF, &buf);
-/*
+                for (i = 0; i < n_buffers; ++i)
+                {
+                        CLEAR(buf);
+                        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+                        buf.memory = V4L2_MEMORY_MMAP;
+                        buf.index = i;
+                        xioctl(fd, VIDIOC_QBUF, &buf);
+                }
+                type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+
+                xioctl(fd, VIDIOC_STREAMON, &type);
+                for (i = 0; i < 1; i++)
+                {
+                        do
+                        {
+                                FD_ZERO(&fds);
+                                FD_SET(fd, &fds);
+
+                                /* Timeout. */
+                                tv.tv_sec = 2;
+                                tv.tv_usec = 0;
+
+                                r = select(fd + 1, &fds, NULL, NULL, &tv);
+                        } while ((r == -1 && (errno = EINTR)));
+                        if (r == -1)
+                        {
+                                perror("select");
+                                return errno;
+                        }
+
+                        CLEAR(buf);
+                        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+                        buf.memory = V4L2_MEMORY_MMAP;
+                        xioctl(fd, VIDIOC_DQBUF, &buf);
+                        /*
                 sprintf(out_name, "out%03d.jpg", i);
                mosquitto_publish(mosq,NULL,"Heartbeat/CAMERA" ,1,enabled,0,false);
                fout = fopen(out_name, "w");
@@ -191,82 +194,72 @@ void message_callback(struct mosquitto *mosq, void *obj, const struct mosquitto_
                 fclose(fout);
 */
 
-	       mosquitto_publish(mosq,NULL,"DATA/Frame" ,buf.bytesused,buffers[buf.index].start,0,false);
+                        mosquitto_publish(mosq, NULL, "DATA/Frame", buf.bytesused, buffers[buf.index].start, 0, false);
 
+                        xioctl(fd, VIDIOC_QBUF, &buf);
+                }
 
-                xioctl(fd, VIDIOC_QBUF, &buf);
+                type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+                xioctl(fd, VIDIOC_STREAMOFF, &type);
+                for (i = 0; i < n_buffers; ++i)
+                        v4l2_munmap(buffers[i].start, buffers[i].length);
+                v4l2_close(fd);
         }
-
-        type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-        xioctl(fd, VIDIOC_STREAMOFF, &type);
-        for (i = 0; i < n_buffers; ++i)
-                v4l2_munmap(buffers[i].start, buffers[i].length);
-        v4l2_close(fd);
-
-
-
-
-
-        }
-
 }
 
 int main(int argc, char *argv[])
 {
-	uint8_t reconnect = true;
-	char clientid[24];
-	struct mosquitto *mosq;
-	int rc = 0;
+        uint8_t reconnect = true;
+        char clientid[24];
+        struct mosquitto *mosq;
+        int rc = 0;
 
-	signal(SIGINT, handle_signal);
-	signal(SIGTERM, handle_signal);
+        signal(SIGINT, handle_signal);
+        signal(SIGTERM, handle_signal);
 
-	mosquitto_lib_init();
+        mosquitto_lib_init();
 
-	memset(clientid, 0, 24);
-	snprintf(clientid, 23, "mysql_log_%d", getpid());
-	mosq = mosquitto_new(clientid, true, 0);
+        memset(clientid, 0, 24);
+        snprintf(clientid, 23, "mysql_log_%d", getpid());
+        mosq = mosquitto_new(clientid, true, 0);
 
+        struct sigaction sact;
 
-    struct sigaction sact;
-    
-    sigemptyset(&sact.sa_mask);
-    sact.sa_flags = 0;
-    sact.sa_handler = sigalrm_handler;
-    sigaction(SIGALRM, &sact, NULL);
+        sigemptyset(&sact.sa_mask);
+        sact.sa_flags = 0;
+        sact.sa_handler = sigalrm_handler;
+        sigaction(SIGALRM, &sact, NULL);
 
+        if (mosq)
+        {
+                mosquitto_connect_callback_set(mosq, connect_callback);
+                mosquitto_message_callback_set(mosq, message_callback);
 
+                rc = mosquitto_connect(mosq, mqtt_host, mqtt_port, 60);
 
-
-	if(mosq){
-		mosquitto_connect_callback_set(mosq, connect_callback);
-		mosquitto_message_callback_set(mosq, message_callback);
-
-	    rc = mosquitto_connect(mosq, mqtt_host, mqtt_port, 60);
-
-		mosquitto_subscribe(mosq, NULL, "Camera/#", 0);
+                mosquitto_subscribe(mosq, NULL, "Camera/#", 0);
                 alarm(1);
-		while(run){
-			rc = mosquitto_loop(mosq, -1, 1);
-                        if (stop==1)
+                while (run)
+                {
+                        rc = mosquitto_loop(mosq, -1, 1);
+                        if (stop == 1)
                         {
-                            
-                            mosquitto_publish(mosq,NULL,"Heartbeat/CAMERA" ,1,enabled,0,false);
-                            stop=0;
-                            alarm(1);
+
+                                mosquitto_publish(mosq, NULL, "Heartbeat/CAMERA", 1, enabled, 0, false);
+                                stop = 0;
+                                alarm(1);
                         }
-			if(run && rc){
-				printf("connection error!\n");
-				sleep(10);
-				mosquitto_reconnect(mosq);
-			}
-               
-		}
-		mosquitto_destroy(mosq);
-	}
+                        if (run && rc)
+                        {
+                                printf("connection error!\n");
+                                sleep(10);
+                                mosquitto_reconnect(mosq);
+                        }
+                }
+                mosquitto_destroy(mosq);
+        }
 
-	mosquitto_lib_cleanup();
+        mosquitto_lib_cleanup();
 
-	return rc;
+        return rc;
 }
-
